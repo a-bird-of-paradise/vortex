784986f4c87eda843cf1c849bbc712977a089821
Mark Douglas <mark.t.douglas@gmail.com>
2025-02-15 09:48:00 +0000

    Adapting onto complex datatypes

--- sheet-rollup/src/main.rs
+++ sheet-rollup/src/main.rs
@@ -1,6 +1,9 @@
-use std::{f64, fs::File};
+use core::f64;
+use std::fs::File;
 use std::io::Write;
-use std::thread;
+use num::pow::Pow;
+use num::Complex;
+use std::f64::consts::PI;
 
 fn main() {
     let n_points: usize = 800;
@@ -8,8 +11,8 @@ fn main() {
     let n_steps: usize = 1000;
     let epsilon: f64 = 0.05;  
 
-    let mut x = init_x(n_points);
-    let mut y = init_y(n_points);
+    let mut z = init_z(n_points,3.0);
+    let gamma = init_gamma(n_points,3.0);
 
     let mut file = std::fs::File::create("data.csv").unwrap();
 
@@ -17,159 +20,93 @@ fn main() {
     {
         println!("{}",i);
         //if i % 10 == 0 {
-            print(&x,&y,n_points,i, &mut file);
+            print(&z,n_points,i, &mut file);
         //}
 
-        let (new_x, new_y) = thread::scope(|s| {
-
-            let join1 = s.spawn(|| rk4_x(&x,&y,n_points,dt,epsilon));
-            let join2 = s.spawn(|| rk4_y(&x,&y,n_points,dt,epsilon));
-
-            (join1.join(), join2.join())
-
-        });
-
-        x = new_x.unwrap();
-        y = new_y.unwrap();
+        z = rk4(&z,&gamma,n_steps,dt,epsilon);
         
     }
 
 }
 
-fn print(x: &[f64], y: &[f64], n: usize, t: usize, file: &mut File) 
+fn print(z: &[Complex<f64>], n: usize, t: usize, file: &mut File) 
 {
     for i in 0..n {
-            let _ = writeln!(file,"{},{},{},{}", t, i, x[i], y[i]);
+        let _ = writeln!(file,"{},{},{},{}", t, i, z[i].re, z[i].im);
     }
 }
 
-fn step_x(x: &Vec<f64>, y: &Vec<f64>, n: usize, h: f64, epsilon: f64) -> Vec<f64>
+fn init_z(n: usize, k: f64) -> Vec<Complex<f64>>
 {
-    let dx = f_x(x,y,n,epsilon);
-    let answer: Vec<f64> = (0..n).map(|i| x[i] + h * dx[i]).collect();
-    answer
-}
-
-fn step_y(x: &Vec<f64>, y: &Vec<f64>, n: usize, h: f64, epsilon: f64) -> Vec<f64>
-{
-    let dy = f_y(x,y,n,epsilon);
-    let answer: Vec<f64> = (0..n).map(|i| y[i] + h * dy[i]).collect();
-    answer
-}
-
-fn rk4_x(x: &[f64], y: &[f64], n: usize, h: f64, epsilon: f64) -> Vec<f64>
-{
-    let k1 : Vec<f64> = f_x(x,y,n,epsilon).iter().map(|u| u * h).collect();
-    let k0_k12: Vec<f64> = x.iter().zip(k1.iter()).map(|(a,b)| a + b / 2.0).collect();
-    let k2 : Vec<f64> = f_x(&k0_k12,y,n,epsilon).iter().map(|u| u * h).collect();
-    let k0_k22 : Vec<f64> = x.iter().zip(k2.iter()).map(|(a,b)| a + b / 2.0).collect();
-    let k3 : Vec<f64> = f_x(&k0_k22,y,n,epsilon).iter().map(|u| u * h).collect();
-    let k0_k3 : Vec<f64> = x.iter().zip(k3.iter()).map(|(a,b)| a + b).collect();
-    let k4 : Vec<f64> = f_x(&k0_k3, y,n,epsilon).iter().map(|u| u * h).collect();
-
-    k1.iter().zip(k2.iter()).map(|(a,b)| a + 2.0 * b)
-        .zip(k3.iter()).map(|(a,b)| a + 2.0 * b)
-        .zip(k4.iter()).map(|(a,b)| (a + b) / 6.0) 
-        .zip(x.iter()).map(|(a,b)| a + b).collect()
-}
-
-fn rk4_y(x: &[f64], y: &[f64], n: usize, h: f64, epsilon: f64) -> Vec<f64>
-{
-    let k1 : Vec<f64> = f_y(x,y,n,epsilon).iter().map(|u| u * h).collect();
-    let k0_k12: Vec<f64> = y.iter().zip(k1.iter()).map(|(a,b)| a + b / 2.0).collect();
-    let k2 : Vec<f64> = f_y(x,&k0_k12,n,epsilon).iter().map(|u| u * h).collect();
-    let k0_k22 : Vec<f64> = y.iter().zip(k2.iter()).map(|(a,b)| a + b / 2.0).collect();
-    let k3 : Vec<f64> = f_y(x,&k0_k22,n,epsilon).iter().map(|u| u * h).collect();
-    let k0_k3 : Vec<f64> = y.iter().zip(k3.iter()).map(|(a,b)| a + b).collect();
-    let k4 : Vec<f64> = f_y(x,&k0_k3,n,epsilon).iter().map(|u| u * h).collect();
-
-    k1.iter().zip(k2.iter()).map(|(a,b)| a + 2.0 * b)
-        .zip(k3.iter()).map(|(a,b)| a + 2.0 * b)
-        .zip(k4.iter()).map(|(a,b)| (a + b) / 6.0) 
-        .zip(y.iter()).map(|(a,b)| a + b).collect()
-}
-
-fn init_x(n: usize) -> Vec<f64> {
-    let d_xi = 1.0 / (0.0 + (n as f64));
+    let d_xi = (n as f64).recip();
     let mut xi = 0.0;
 
-    let mut answer : Vec<f64> = Vec::new();
+    let mut answer: Vec<Complex<f64>> = Vec::new();
 
     for _i in 1..=n 
     {
         xi = xi + d_xi;
-        answer.push(xi + 0.01 * (2.0 * 3.0 * std::f64::consts::PI * xi).sin() );
+        answer.push(Complex::new(xi,-0.01 * (2.0 * k * PI * xi).sin()));
     }
     answer
 }
 
-fn init_y(n: usize) -> Vec<f64> {
-    let d_xi = 1.0 / (0.0 + (n as f64));
+fn init_gamma(n: usize, _k: f64) -> Vec<f64>
+{
+    let d_xi = (n as f64).recip();
     let mut xi = 0.0;
 
-    let mut answer : Vec<f64> = Vec::new();
+    let mut answer: Vec<f64> = Vec::new();
 
     for _i in 1..=n 
     {
         xi = xi + d_xi;
-        answer.push(-0.01 * (2.0 * 3.0 * std::f64::consts::PI * xi).sin() );
+        answer.push(1.0);
     }
     answer
 }
 
-fn f_x(x: &[f64], y: &[f64], n: usize, epsilon: f64) -> Vec<f64> 
+fn rk4(z: &[Complex<f64>], gamma: &[f64], n: usize, h: f64, epsilon: f64) -> Vec<Complex<f64>>
 {
-    let mut answer: Vec<f64> = vec![];
-    answer.resize(n, 0.0);
+    let k1 : Vec<Complex<f64>> = f_z(z,gamma,n,epsilon).iter().map(|u| u * h).collect();
+    let k0_k12: Vec<Complex<f64>> = z.iter().zip(k1.iter()).map(|(a,b)| a + b / 2.0).collect();
+    let k2 : Vec<Complex<f64>> = f_z(&k0_k12, gamma,n,epsilon).iter().map(|u| u * h).collect();
+    let k0_k22 : Vec<Complex<f64>> = z.iter().zip(k2.iter()).map(|(a,b)| a + b / 2.0).collect();
+    let k3 : Vec<Complex<f64>> = f_z(&k0_k22,gamma,n,epsilon).iter().map(|u| u * h).collect();
+    let k0_k3 : Vec<Complex<f64>> = z.iter().zip(k3.iter()).map(|(a,b)| a + b).collect();
+    let k4 : Vec<Complex<f64>> = f_z(&k0_k3,gamma,n,epsilon).iter().map(|u| u * h).collect();
 
-    for i in 0..n
-    {
-        let mut accum: f64 = 0.0;
-        for j in 0..n 
-        {
-            if i == j {
-                continue;
-            }
-            let mut temp: f64; 
-            temp = ( (y[i] - y[j]) * 2.0 * std::f64::consts::PI).sinh();
-            temp /= 
-                ((y[i] - y[j]) * 2.0 * std::f64::consts::PI).cosh() 
-                - ((x[i] - x[j]) * 2.0 * std::f64::consts::PI).cos() 
-                + epsilon * epsilon
-            ;
-            accum += temp;
-        }
-        accum /= 2.0 * (n as f64);
-        accum *= -1.0;
-        answer[i] = accum;
-    }
-    answer
+    k1.iter().zip(k2.iter()).map(|(a,b)| a + 2.0 * b)
+        .zip(k3.iter()).map(|(a,b)| a + 2.0 * b)
+        .zip(k4.iter()).map(|(a,b)| (a + b) / 6.0) 
+        .zip(z.iter()).map(|(a,b)| a + b).collect()
 }
 
-fn f_y(x: &[f64], y: &[f64], n: usize, epsilon: f64) -> Vec<f64> 
+fn f_z(z: &[Complex<f64>], gamma: &[f64], n: usize, epsilon: f64) -> Vec<Complex<f64>>
 {
-    let mut answer: Vec<f64> = vec![];
-    answer.resize(n, 0.0);
+
+    let mut answer: Vec<Complex<f64>> = Vec::new();
+    answer.reserve_exact(n);
 
     for i in 0..n
     {
-        let mut accum: f64 = 0.0;
-        for j in 0..n 
+        let mut accum: Complex<f64> = Complex::new(0.0,0.0);
+        for j in 0..n
         {
-            if i == j {
+            if i == j { 
                 continue;
             }
-            let mut temp: f64; 
-            temp = ( (x[i] - x[j]) * 2.0 * std::f64::consts::PI).sin();
-            temp /= 
-                ((y[i] - y[j]) * 2.0 * std::f64::consts::PI).cosh() 
-                - ((x[i] - x[j]) * 2.0 * std::f64::consts::PI).cos() 
-                + epsilon * epsilon
-            ;
-            accum += temp;
+            
+            let dz = z[i] -
+             z[j];
+
+            let temp_real = (-2.0 * PI * dz.im).sinh();
+            let temp_im = (2.0 * PI * dz.re).sin();
+            let temp_denom = (dz.im * 2.0 * PI).cosh() - (dz.re * 2.0 * PI).cos() + epsilon.pow(2);
+
+            accum += Complex::new(temp_real / temp_denom, temp_im / temp_denom) * gamma[j];
         }
         accum /= 2.0 * (n as f64);
-        accum *= 1.0;
         answer[i] = accum;
     }
     answer
