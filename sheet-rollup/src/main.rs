ebc530a52b0fbdce0eb1c0d478e71b38ceddaa4b
Mark Douglas <mark.t.douglas@gmail.com>
2025-02-22 08:11:40 +0000

    Birkhoff-Rott equations, periodic in the real axis

--- sheet-rollup/src/main.rs
+++ sheet-rollup/src/main.rs
@@ -4,35 +4,49 @@ use std::io::Write;
 use num::pow::Pow;
 use num::Complex;
 use std::f64::consts::PI;
+use rayon::prelude::*;
 
 fn main() {
-    let n_points: usize = 800;
-    let dt: f64 = 0.0005;
-    let n_steps: usize = 1000;
+    let n_points: usize = 16000;
+    let dt: f64 = 0.002;
+    let n_steps: usize = 2000;
     let epsilon: f64 = 0.05;  
 
-    let mut z = init_z(n_points,3.0);
-    let gamma = init_gamma(n_points,3.0);
+    let mut z = init_z(n_points,1.0);
+    let gamma = init_gamma(n_points,1.0);
 
     let mut file = std::fs::File::create("data.csv").unwrap();
+
+    let mut nudge: Vec<Complex<f64>> = Vec::new();
+    nudge.resize(n_points, Complex::new(0.0,0.0));
 
     for i in 0..n_steps
     {
         println!("{}",i);
         //if i % 10 == 0 {
-            print(&z,n_points,i, &mut file);
+            print(&z,&nudge, n_points,i, &mut file);
         //}
 
         z = rk4(&z,&gamma,n_points,dt,epsilon);
+
+        for j in 0..n_points {
+            if z[j].re > 1.0 {
+                z[j] -= 1.0;
+                nudge[j] += 1.0;
+            } else if z[j].re < 0.0 {
+                z[j] += 1.0;
+                nudge[j] -= 1.0;
+            }
+        }
         
     }
 
 }
 
-fn print(z: &[Complex<f64>], n: usize, t: usize, file: &mut File) 
+fn print(z: &[Complex<f64>], nudge: &[Complex<f64>], n: usize, t: usize, file: &mut File) 
 {
     for i in 0..n {
-        let _ = writeln!(file,"{},{},{},{}", t, i, z[i].re, z[i].im);
+        let _ = writeln!(file,"{},{},{},{}", t, i, z[i].re + nudge[i].re, z[i].im);
     }
 }
 
@@ -43,15 +57,29 @@ fn init_z(n: usize, k: f64) -> Vec<Complex<f64>>
 
     let mut answer: Vec<Complex<f64>> = Vec::new();
 
-    for _i in 1..=n 
+    for _i in 1..=n/2 
     {
-        xi = xi + d_xi;
-        answer.push(Complex::new(xi,-0.01 * (2.0 * k * PI * xi).sin()));
+        xi = xi + 2.0 * d_xi;
+         answer.push(Complex::new(xi,0.2 * (2.0 * k * PI * xi).sin()));
+        //answer.push(Complex::new(xi + 0.1 * (2.0 * k * PI * xi).sin(),0.2 * (2.0 * k * PI * xi).sin()));
+        /*answer.push(
+            Complex::new(
+                xi + 0.05 * (2.0 * 2.0 * k * PI * xi).sin(),
+                0.05 * (2.0 * 2.0 * k * PI * xi).sin() * if xi > 0.5 { -1.0 } else { 1.0 }
+            )
+        );*/
+
+        //answer.push(Complex::new(xi, -0.2));
     }
+
+    let i1 : Complex<f64> = Complex::new(0.0, -0.5); // original -0.2; thin -0.1; thick -0.5
+
+    let temp : Vec<Complex<f64>> = answer.clone().iter().map(|z| z + i1).collect();
+    temp.iter().for_each(|z| answer.push(*z));
     answer
 }
 
-fn init_gamma(n: usize, _k: f64) -> Vec<f64>
+fn init_gamma(n: usize, k: f64) -> Vec<f64>
 {
     let d_xi = (n as f64).recip();
     let mut xi = 0.0;
@@ -61,6 +89,9 @@ fn init_gamma(n: usize, _k: f64) -> Vec<f64>
     for _i in 1..=n 
     {
         xi = xi + d_xi;
+        //answer.push((2.0 * k * PI * xi).sin());
+        //answer.push((2.0 * 2.0 * k * PI * xi).sin() * if xi > 0.5 { -1.0 } else { 1.0 });
+        answer.push(1.0);
         answer.push(1.0);
     }
     answer
@@ -85,27 +116,30 @@ fn rk4(z: &[Complex<f64>], gamma: &[f64], n: usize, h: f64, epsilon: f64) -> Vec
 fn f_z(z: &[Complex<f64>], gamma: &[f64], n: usize, epsilon: f64) -> Vec<Complex<f64>>
 {
     let mut answer: Vec<Complex<f64>> = Vec::new();
-    answer.resize(n, Complex::new(0.0,0.0));
 
-    for i in 0..n
-    {
-        let mut accum: Complex<f64> = Complex::new(0.0,0.0);
-        for j in 0..n
+    (0..n)
+        .into_par_iter()
+        .map(|i|
         {
-            if i == j { 
-                continue;
+            let mut accum: Complex<f64> = Complex::new(0.0,0.0);
+            for j in 0..n
+            {
+                if i == j { 
+                   continue;
+                }
+                
+                let dz = z[i] - z[j];
+
+                let temp_real = (-2.0 * PI * dz.im).sinh();
+                let temp_im = (2.0 * PI * dz.re).sin();
+                let temp_denom = (dz.im * 2.0 * PI).cosh() - (dz.re * 2.0 * PI).cos() + epsilon.pow(2);
+
+                accum += Complex::new(temp_real / temp_denom, temp_im / temp_denom) * gamma[j];
             }
-            
-            let dz = z[i] - z[j];
-
-            let temp_real = (-2.0 * PI * dz.im).sinh();
-            let temp_im = (2.0 * PI * dz.re).sin();
-            let temp_denom = (dz.im * 2.0 * PI).cosh() - (dz.re * 2.0 * PI).cos() + epsilon.pow(2);
-
-            accum += Complex::new(temp_real / temp_denom, temp_im / temp_denom) * gamma[j];
+            accum /= 2.0 * (n as f64);
+            accum
         }
-        accum /= 2.0 * (n as f64);
-        answer[i] = accum;
-    }
+    )
+    .collect_into_vec(&mut answer);
     answer
 }
